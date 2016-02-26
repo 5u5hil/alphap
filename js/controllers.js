331c0b37375b2d85a13b18135a4dfcbf11497d8a
leenaNi <leena@infiniteit.biz>
2016-02-26 20:42:26 +0530

    aaaa

--- js/controllers.js
+++ js/controllers.js
@@ -482,16 +482,7 @@ angular.module('your_app_name.controllers', ['ionic', 'ngCordova'])
                         }
                     });
                 }
-                function removeFile() {
-                    console.log('remove');
-                    jQuery('.img').val('');
-                    jQuery('#image-holder').empty();
-                }
-                function removeCamFile() {
-                    console.log('remove');
-                    jQuery('#camera-status').empty();
-                    $scope.image = [];
-                }
+                
                 function getImgUrl(imageName) {
                     var name = imageName.substr(imageName.lastIndexOf('/') + 1);
                     var trueOrigin = cordova.file.dataDirectory + name;
